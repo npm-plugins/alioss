040a768d22e894ce291e98ad93878a3dc363f68a
Min Zhang <zhangmin@MindeMacBook-Pro.local>
2019-01-15 16:29:49 +0800

    delete info and hue

--- alioss.d.ts
+++ alioss.d.ts
@@ -10,29 +10,21 @@ interface Alioss {
    * 处理结果
    */
   result(): string
-  /**
-   * 图片信息
-   */
-  info(): AliossInfo
-  /**
-   * 获取图片的平均色调
-   */
-  averageHue(): AliossHue
 }
 
-interface AliossInfo {
-  FileSize: InfoFormat,
-  Format: InfoFormat,
-  ImageHeight: InfoFormat,
-  ResolutionUnit: InfoFormat,
-  XResolution: InfoFormat,
-  YResolution: InfoFormat
-}
+// interface AliossInfo {
+//   FileSize: InfoFormat,
+//   Format: InfoFormat,
+//   ImageHeight: InfoFormat,
+//   ResolutionUnit: InfoFormat,
+//   XResolution: InfoFormat,
+//   YResolution: InfoFormat
+// }
 
-interface InfoFormat {
-  value: string
-}
+// interface InfoFormat {
+//   value: string
+// }
 
-interface AliossHue {
-  RGB: string
-}
+// interface AliossHue {
+//   RGB: string
+// }
--- src/core/init.js
+++ src/core/init.js
@@ -5,7 +5,7 @@
  * @returns {import("../../alioss").Alioss}
  */
 export default function(url, type = 'image') {
-  const source = `${url}?x-oss-process=${type}`
+  // const source = `${url}?x-oss-process=${type}`
   let target = `${url}?x-oss-process=${type}`
 
   function process(handler) {
@@ -25,19 +25,17 @@ export default function(url, type = 'image') {
     return target
   }
 
-  function info() {
-    return source + '/info'
-  }
+  // function info() {
+  //   return source + '/info'
+  // }
 
-  function averageHue() {
-    return source + '/average-hue'
-  }
+  // function averageHue() {
+  //   return source + '/average-hue'
+  // }
 
   const modules = {
     process,
-    result,
-    info,
-    averageHue
+    result
   }
 
   return modules
